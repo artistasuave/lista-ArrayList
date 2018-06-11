83743fe6365db12080c67285a68cac5f8bc4704a
Michelle Rogério <themicaloo@gmail.com>
2018-06-11 08:42:56 -0300

    7

--- ExerciciosArrayList/src/Exercicio07.java
+++ ExerciciosArrayList/src/Exercicio07.java
@@ -10,29 +10,46 @@ public class Exercicio07 {
     public static void main(String[] args) {
         ArrayList<String> nomes = new ArrayList<>();
 
-        for (int i = 0; i < 25; i++) {
-            nomes.add(JOptionPane.showInputDialog("Digite o " + (i + 1) + "º nome"));
+        for (int i = 0; i < 5; i++) {
+            nomes.add(JOptionPane.showInputDialog("Digite o " + (i + 1) + "º nome").trim().toUpperCase());
         }
 
         String apresentaNomes = "";
-        for (int i = 0; i < 25; i++) {
+        for (int i = 0; i < nomes.size(); i++) {
             apresentaNomes += nomes.get(i) + "\n";
         }
-        JOptionPane.showMessageDialog(null, "Os nomes cadastrados são: " + apresentaNomes);
 
         String maiorNome = "";
         String menorNome = "";
+        int contadorPaulo = 0, contadorAna = 0, contadorVogal = 0;
 
-        for (int i = 0; i < 25; i++) {
+        for (int i = 0; i < nomes.size(); i++) {
             if (nomes.get(i).length() > maiorNome.length()) {
-                maiorNome = nomes(i);
+                maiorNome = nomes.get(i);
             }
+            if (nomes.get(i).length() < maiorNome.length()) {
+                menorNome = nomes.get(i);
+            }
+            if (nomes.get(i).equals("PAULO")) {
+                contadorPaulo++;
+            }
+            if (nomes.get(i).equals("ANA")) {
+                contadorAna++;
+            }
+            /*
+            char vogal = nomes.get(i).charAt(0);
+            if ((vogal == 'a') || (vogal == 'e') || (vogal == 'i') || (vogal == 'o') || (vogal == 'u')) {
+                contadorVogal++;
+            }*/
         }
-//maior nome
-        //menor nome
-        //qtd paulo
-        //qtd ana
-        //qtd pessoas primeira letra vogal
+
+        JOptionPane.showMessageDialog(null,
+                "Os nomes cadastrados são: " + apresentaNomes
+                + "\nO maior nome é: " + maiorNome
+                + "\nO menor nome é: " + menorNome
+                + "\nA quantidade de \"Paulo's\" cadastrados é: " + contadorPaulo
+                + "\nA quantidade de \"Ana's\" cadastradas é: " + contadorAna
+                + "\nA quantidade de nomes cadastrados que iniciam-se com vogal é: " + contadorVogal);
 
     }
 }
